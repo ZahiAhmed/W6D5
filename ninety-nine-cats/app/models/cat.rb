4eaa131e77c96ffeec81e9a7e8c201ba7983dc4b
zahiahmed <zahi.ahmed33@gmail.com>
2022-12-16 16:04:26 -0500

    created cats table

---
+++ ninety-nine-cats/app/models/cat.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint           not null, primary key
+#  birthdate   :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Cat < ApplicationRecord
+    
+end
